c905a4c78cb6f3f2a5e7a33a7368678324fb3087
Shree419 <shreyanshguduru@gmail.com>
2026-06-04 21:33:09 -0500

    Create VariablePractice.java
    Learning variables. This week, I am aiming to learn how to code gamepad/teleop and learn more about autonomous coding.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariablePractice.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class VariablePractice extends OpMode {
+
+    @Override
+    public void init() {
+        int TeamNumber = 19448;
+        double MotorSpeed = 0.75;
+        boolean Intakeon = true;
+
+
+        telemetry.addData("Team Number", TeamNumber);
+        telemetry.addData("MotorSpeed", MotorSpeed);
+        telemetry.addData("Intake State", Intakeon);
+    }
+}
